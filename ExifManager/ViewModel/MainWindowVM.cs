9f7d5eafafb16154f2b54b00d06db86458135c4d
Charles Zhang <master@weilog.net>
2019-11-04 19:27:34 +0800

    添加项目文件。

---
+++ ExifManager/ViewModel/MainWindowVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FileManager.ViewModel
+{
+    public class MainWindowVM
+    {
+        //private readonly WrapPanel _mainContentPanel;
+
+        private readonly Window _currentWindow;
+
+        public MainWindowVM(Window window) {
+
+            _currentWindow = window;
+        }
+
+    }
+}
