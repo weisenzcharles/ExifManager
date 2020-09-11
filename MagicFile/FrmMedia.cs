c899aa158adac4d766cf24b356e2c3e94985c17e
Charles Zhang <master@weilog.net>
2020-09-11 17:57:05 +0800

    增加窗体

---
+++ MagicFile/FrmMedia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagicFile
+{
+    public partial class FrmMedia : Form
+    {
+        public FrmMedia()
+        {
+            InitializeComponent();
+        }
+    }
+}
